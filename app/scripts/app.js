389853b25b0397a132d92f733901458b47c4d0d2
cphillips <clint.phillips@gmail.com>
2015-12-27 08:43:53 -0700

    boilerplate

---
+++ app/scripts/app.js
@@ -0,0 +1,9 @@
+(function(/*document*/) {
+  'use strict';
+
+  // See https://github.com/Polymer/polymer/issues/1381
+  window.addEventListener('WebComponentsReady', function() {
+    // imports are loaded and elements have been registered
+  });
+
+})(document);
